1cc8bb8910c3b1be155f2ea4fb89c4ce14d218a6
BrownKnight <amanrk@hotmail.co.uk>
2020-11-01 16:02:05 +0000

    Show tab panel on click of tab

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log("hello World!");
+function setTabClickEventListener(tabNumber) {
+    const tabSelectButton = document.getElementById(`tab-select-${tabNumber}`);
+    if (tabSelectButton === null) {
+        // TODO throw exception / log error?
+        return;
+    }
+    tabSelectButton.addEventListener("click", showTab.bind(null, tabNumber));
+}
+function showTab(tabNumber) {
+    const tabPanels = document.getElementsByClassName("tab-panel");
+    console.log(`Showing tab ${tabNumber}`);
+    for (let tabPanel of tabPanels) {
+        if (tabPanel.id === `tab-panel-${tabNumber}`) {
+            tabPanel.classList.remove("hidden");
+        }
+        else {
+            tabPanel.classList.add("hidden");
+        }
+    }
+}
+// Setup required function calls when the document is loaded
+setTabClickEventListener("1");
+setTabClickEventListener("2");
