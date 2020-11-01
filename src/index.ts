e87378bf51a5b81b0e15553d94138a0f1bd1a866
BrownKnight <amanrk@hotmail.co.uk>
2020-11-01 16:06:59 +0000

    Set tab buttons to active state when clicked

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,25 @@ function setTabClickEventListener(tabNumber: string) {
 
 function showTab(tabNumber: string) {
   const tabPanels = document.getElementsByClassName("tab-panel");
-  console.log(`Showing tab ${tabNumber}`)
 
+  // Hide all other tab panels, and show the one specified by removed the hidden class
   for (let tabPanel of tabPanels) {
     if (tabPanel.id === `tab-panel-${tabNumber}`) {
       tabPanel.classList.remove("hidden");
     }
     else {
       tabPanel.classList.add("hidden")
+    }
+  }
+
+  // Set the clicked tab button to its active state by adding the class
+  const tabButtons = document.getElementsByClassName("tab");
+  for (let tabButton of tabButtons) {
+    if (tabButton.id === `tab-select-${tabNumber}`) {
+      tabButton.classList.add("active");
+    }
+    else {
+      tabButton.classList.remove("active")
     }
   }
 }
